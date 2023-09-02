c5957341d39b3763f7822d853e15b38313558619
jazminmotauteq <125316302+jazminmotauteq@users.noreply.github.com>
2023-09-02 17:44:31 -0500

    Jazmin Mora
    DiseñAR LA INTETFAZ GRÀFICA DE USUARIO SIMILAR A LA DE app movil MUY POPULAR USANDO. NAVIGATION VIEW

---
+++ app/src/main/java/com/example/NavegationView/MainActivityNatview.java
@@ -0,0 +1,37 @@
+package com.example.clasepracticaenero2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivityNatview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_natview);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.iconhome);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    @Override
+public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+        case android.R.id.home:
+            DrawerLayout drawerLayout= (DrawerLayout)findViewById(R.id.drawer_layout);
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+      }
+}
+
+}
